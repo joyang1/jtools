4328b94912ef7aaa12e664a463c75021d1d4e8c5
joyang1 <tingzai.yang@gmail.com>
2022-04-17 22:08:35 +0800

    1、TimeTools更新及单测。

--- src/main/java/cn/tommyyang/jtools/time/TimeTools.java
+++ src/main/java/cn/tommyyang/jtools/time/TimeTools.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -44,6 +45,11 @@ public class TimeTools {
      * 格式化时间：精确到秒钟
      */
     public static final String DATE_PATTERN_SS = "yyyyMMdd HH:mm:ss";
+
+    /**
+     * 格式化时间：精确到毫秒
+     */
+    public static final String DATE_PATTERN_SS_SSS = "yyyyMMdd HH:mm:ss SSS";
 
     /**
      * 格式化时间：精确到秒钟
@@ -107,5 +113,212 @@ public class TimeTools {
         return date;
     }
 
+    /**
+     * 指定时间在当前时间之前
+     *
+     * @param date
+     * @return
+     */
+    public static boolean timeBeforeNow(Date date) {
+        if (null == date) {
+            return Boolean.FALSE;
+        }
 
+        return date.compareTo(new Date()) < 0;
+    }
+
+    /**
+     * 指定时间在当前时间之后
+     *
+     * @param date
+     * @return
+     */
+    public static boolean timeAfterNow(Date date) {
+        if (null == date) {
+            return Boolean.FALSE;
+        }
+
+        return date.compareTo(new Date()) > 0;
+    }
+
+    /**
+     * 为传入日期添加年份
+     *
+     * @param date
+     * @param year
+     * @return
+     */
+    public static Date addYear(Date date, int year) {
+        if (null == date) {
+            return null;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.YEAR, year);
+
+        return calendar.getTime();
+    }
+
+    /**
+     * 为传入日期添加月份
+     *
+     * @param date
+     * @param month
+     * @return
+     */
+    public static Date addMonth(Date date, int month) {
+        if (null == date) {
+            return null;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MONTH, month);
+
+        return calendar.getTime();
+    }
+
+    /**
+     * 为传入日期添加天数
+     *
+     * @param date
+     * @param day
+     * @return
+     */
+    public static Date addDay(Date date, int day) {
+        if (null == date) {
+            return null;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        // DAY_OF_MONTH、DAY_OF_YEAR、DAY_OF_WEEK、DAY_OF_WEEK_IN_MONTH的区别，可以参考：
+        calendar.add(Calendar.DAY_OF_MONTH, day);
+
+        return calendar.getTime();
+    }
+
+    /**
+     * 为传入日期添加小时
+     *
+     * @param date
+     * @param hour
+     * @return
+     */
+    public static Date addHour(Date date, int hour) {
+        if (null == date) {
+            return null;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        // HOUR、HOUR_OF_DAY的区别，可以参考：
+        calendar.add(Calendar.HOUR_OF_DAY, hour);
+
+        return calendar.getTime();
+    }
+
+    /**
+     * 为传入日期添加分钟
+     *
+     * @param date
+     * @param minute
+     * @return
+     */
+    public static Date addMinute(Date date, int minute) {
+        if (null == date) {
+            return null;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MINUTE, minute);
+
+        return calendar.getTime();
+    }
+
+    /**
+     * 为传入日期添加秒数
+     *
+     * @param date
+     * @param second
+     * @return
+     */
+    public static Date addSecond(Date date, int second) {
+        if (null == date) {
+            return null;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.SECOND, second);
+
+        return calendar.getTime();
+    }
+
+    /**
+     * 为传入日期添加毫秒数
+     *
+     * @param date
+     * @param milliSecond
+     * @return
+     */
+    public static Date addMilliSecond(Date date, int milliSecond) {
+        if (null == date) {
+            return null;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MILLISECOND, milliSecond);
+
+        return calendar.getTime();
+    }
+
+    /**
+     * 获取接下来N天后的00:00:00 000
+     *
+     * @param date
+     * @param n
+     * @return
+     */
+    public static Date getNextNDayHeadTime(Date date, int n) {
+        if (null == date) {
+            return null;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        calendar.add(Calendar.DAY_OF_MONTH, n);
+
+        return calendar.getTime();
+    }
+
+    /**
+     * 获取接下来N天后的23:59:59 999
+     *
+     * @param date
+     * @param n
+     * @return
+     */
+    public static Date getNextNDayEndTime(Date date, int n) {
+        if (null == date) {
+            return null;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 999);
+        calendar.add(Calendar.DAY_OF_MONTH, n);
+
+        return calendar.getTime();
+    }
 }
--- src/test/java/cn/tommyyang/jtools/time/TimeToolsTest.java
+++ src/test/java/cn/tommyyang/jtools/time/TimeToolsTest.java
@@ -13,20 +13,38 @@ import java.util.Date;
  */
 public class TimeToolsTest {
 
+    private final static String STR_DATE_20220410 = "20220410";
+
+    private final static String STR_DATE_2022041001 = "20220410 01";
+
+    private final static String STR_DATE_202204100001 = "20220410 00:01";
+
+    private final static String STR_DATE_20220410000001 = "20220410 00:00:01";
+
+    private final static String STR_DATE_20220410000000001 = "20220410 00:00:00 001";
+
+    private final static String STR_DATE_20220411 = "20220411";
+
+    private final static String STR_DATE_20220510 = "20220510";
+
+    private final static String STR_DATE_20230410 = "20230410";
+
+
     /**
      * 测试日期字符串转换成日期
      */
     @Test
     public void testToDate() {
-        String strDate = "20220410";
-        Date date = TimeTools.toDate(strDate, TimeTools.DATE_PATTERN);
-        Assert.assertEquals(strDate, TimeTools.dateFormat(date, TimeTools.DATE_PATTERN));
+        Date date = TimeTools.toDate(STR_DATE_20220410, TimeTools.DATE_PATTERN);
+        Assert.assertEquals(STR_DATE_20220410, TimeTools.dateFormat(date, TimeTools.DATE_PATTERN));
     }
 
+    /**
+     * 测试格式转换
+     */
     @Test
     public void testDateFormat() {
-        String strDate = "20220410";
-        Date date = TimeTools.toDate(strDate, TimeTools.DATE_PATTERN);
+        Date date = TimeTools.toDate(STR_DATE_20220410, TimeTools.DATE_PATTERN);
 
         String dateDay = "2022-04-10";
         String dateHH = "20220410 00";
@@ -36,10 +54,110 @@ public class TimeToolsTest {
 
         Assert.assertEquals(TimeTools.dateFormat(date), dateSS);
         Assert.assertEquals(TimeTools.dateFormat(date, TimeTools.DATE_PATTERN_DAY), dateDay);
-        Assert.assertEquals(TimeTools.dateFormat(date, TimeTools.DATE_PATTERN), strDate);
+        Assert.assertEquals(TimeTools.dateFormat(date, TimeTools.DATE_PATTERN), STR_DATE_20220410);
         Assert.assertEquals(TimeTools.dateFormat(date, TimeTools.DATE_PATTERN_HH), dateHH);
         Assert.assertEquals(TimeTools.dateFormat(date, TimeTools.DATE_PATTERN_MM), dateMM);
         Assert.assertEquals(TimeTools.dateFormat(date, TimeTools.DATE_PATTERN_SESSION), dateSession);
     }
 
+    /**
+     * 测试指定时间在当前时间之前
+     */
+    @Test
+    public void testTimeBeforeNow() {
+        Assert.assertTrue(TimeTools.timeBeforeNow(TimeTools.toDate(STR_DATE_20220410, TimeTools.DATE_PATTERN)));
+    }
+
+    /**
+     * 测试指定时间在当前时间之后
+     */
+    @Test
+    public void testTimeAfterNow() {
+        Assert.assertTrue(TimeTools.timeAfterNow(TimeTools.addDay(new Date(), 1)));
+    }
+
+    /**
+     * 测试添加指定年份
+     */
+    @Test
+    public void testAddYear() {
+        Date date20220410 = TimeTools.toDate(STR_DATE_20220410, TimeTools.DATE_PATTERN);
+        Date date20230410 = TimeTools.toDate(STR_DATE_20230410, TimeTools.DATE_PATTERN);
+
+        Assert.assertTrue(TimeTools.addYear(date20220410, 1)
+                .compareTo(date20230410) == 0);
+    }
+
+    /**
+     * 测试添加指定月份
+     */
+    @Test
+    public void testAddMonth() {
+        Date date20220410 = TimeTools.toDate(STR_DATE_20220410, TimeTools.DATE_PATTERN);
+        Date date20220510 = TimeTools.toDate(STR_DATE_20220510, TimeTools.DATE_PATTERN);
+
+        Assert.assertTrue(TimeTools.addMonth(date20220410, 1)
+                .compareTo(date20220510) == 0);
+    }
+
+    /**
+     * 测试添加指定天数
+     */
+    @Test
+    public void testAddDay() {
+        Date date20220410 = TimeTools.toDate(STR_DATE_20220410, TimeTools.DATE_PATTERN);
+        Date date20220411 = TimeTools.toDate(STR_DATE_20220411, TimeTools.DATE_PATTERN);
+
+        Assert.assertTrue(TimeTools.addDay(date20220410, 1)
+                .compareTo(date20220411) == 0);
+    }
+
+    /**
+     * 测试添加指定小时
+     */
+    @Test
+    public void testAddHour() {
+        Date date20220410 = TimeTools.toDate(STR_DATE_20220410, TimeTools.DATE_PATTERN);
+        Date date2022041001 = TimeTools.toDate(STR_DATE_2022041001, TimeTools.DATE_PATTERN_HH);
+
+        Assert.assertTrue(TimeTools.addHour(date20220410, 1)
+                .compareTo(date2022041001) == 0);
+    }
+
+
+    /**
+     * 测试添加指定分钟
+     */
+    @Test
+    public void testAddMinute() {
+        Date date20220410 = TimeTools.toDate(STR_DATE_20220410, TimeTools.DATE_PATTERN);
+        Date date202204100001 = TimeTools.toDate(STR_DATE_202204100001, TimeTools.DATE_PATTERN_MM);
+
+        Assert.assertTrue(TimeTools.addMinute(date20220410, 1)
+                .compareTo(date202204100001) == 0);
+    }
+
+    /**
+     * 测试添加指定秒钟
+     */
+    @Test
+    public void testAddSecond() {
+        Date date20220410 = TimeTools.toDate(STR_DATE_20220410, TimeTools.DATE_PATTERN);
+        Date date20220410000001 = TimeTools.toDate(STR_DATE_20220410000001, TimeTools.DATE_PATTERN_SS);
+
+        Assert.assertTrue(TimeTools.addSecond(date20220410, 1)
+                .compareTo(date20220410000001) == 0);
+    }
+
+    /**
+     * 测试添加指定毫秒
+     */
+    @Test
+    public void testAddMilliSecond() {
+        Date date20220410 = TimeTools.toDate(STR_DATE_20220410, TimeTools.DATE_PATTERN);
+        Date date20220410000001 = TimeTools.toDate(STR_DATE_20220410000000001, TimeTools.DATE_PATTERN_SS_SSS);
+
+        Assert.assertTrue(TimeTools.addMilliSecond(date20220410, 1)
+                .compareTo(date20220410000001) == 0);
+    }
 }
